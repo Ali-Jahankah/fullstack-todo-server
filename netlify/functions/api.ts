e86eab0351342b1a46c9a56f52161986741cbde2
Ali Jahankah <aral2@yahoo.com>
2024-01-23 16:17:59 +0000

    fix netlify

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,16 +1,14 @@
 import express, {
   Express,
   Request,
-  Response,
-  Router
+  Response
 } from "express";
 
 import serverless from "serverless-http";
 import { tasksRouter } from "../../src/routes/tasks.routes";
 
-const router = Router()
 const api: Express = express();
-router.get("/api", (req: Request, res: Response) => {
+tasksRouter.get("/api", (req: Request, res: Response) => {
   res.send("Server is running");
 });
 api.use("/tasks", tasksRouter);
